67a227620239b80fdb47d9152b55ff7326775e0f
Kim Taenam <cjb940103@gmail.com>
2021-12-07 14:13:33 +0900

    svelte 리스트 만들기

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,57 @@
 <script>
-	export let name;
+	import { onMount } from 'svelte';
+	import List from './List.svelte';
+	import Item from './Item.svelte';
+
+	let item;
+	let page;
+
+	const hashchange = async() => {
+		const path = window.location.hash.slice(1);
+		console.log(path);
+
+		if(path.startsWith('/item')) {
+			const id = path.slice(6);
+			item = await fetch(`https://node-hnapi.herokuapp.com/item/${id}`).then(r => r.json());
+			
+			window.scrollTo(0, 0);
+		} else if (path.startsWith('/top')) {
+			page = +path.slice(5);
+			item = null;
+		} else {
+			window.location.hash = "/top/1";
+		}
+	}
+
+	onMount(hashchange);
 </script>
 
+<svelte:window on:hashchange={hashchange}/>
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{#if item}
+		<Item {item} returnTo="#/top/{page}"/>
+	{:else if page}
+		<List {page}/>
+	{/if}
 </main>
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
+		position: relative;
+		max-width: 800px;
 		margin: 0 auto;
+		min-height: 101vh;
+		padding: 1em;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	main :global(.meta) {
+		color: #999;
+		font-size: 12px;
+		margin: 0 0 1em 0;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	main :global(a) {
+		color: rgb(0,0,150);
 	}
 </style>
