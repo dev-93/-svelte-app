7e0da22f524cbbdac85c36cac3d8b34a5c9b1dfe
Kim Taenam <cjb940103@gmail.com>
2021-12-07 19:59:19 +0900

    make todo App

--- src/App.svelte
+++ src/App.svelte
@@ -1,56 +1,92 @@
 <script>
-	import { onMount } from 'svelte';
-	import List from './List.svelte';
-	import Item from './Item.svelte';
-
-	let item;
-	let page;
-
-	const hashchange = async() => {
-		const path = window.location.hash.slice(1);
-
-		if(path.startsWith('/item')) {
-			const id = path.slice(6);
-			item = await fetch(`https://node-hnapi.herokuapp.com/item/${id}`).then(r => r.json());
-			
-			window.scrollTo(0, 0);
-		} else if (path.startsWith('/top')) {
-			page = +path.slice(5);
-			item = null;
-		} else {
-			window.location.hash = "/top/1";
-		}
+	const add = (input) => {
+		console.log(input.value);
 	}
-
-	onMount(hashchange);
 </script>
 
-<svelte:window on:hashchange={hashchange}/>
 
-<main>
-	{#if item}
-		<Item {item} returnTo="#/top/{page}"/>
-	{:else if page}
-		<List {page}/>
-	{/if}
-</main>
+<div class="board">
+	<input 
+		class="new-todo"
+		placeholder="what needs to be done?"
+		on:keydown={e => e.key === "Enter" && add(e.target)}
+	/>
+
+	<div class="left">
+		<h2>todo</h2>
+		<label>
+			<input type="checkbox"/>
+			<button>x</button>
+		</label>
+	</div>
+
+	<div class="right">
+		<h2>done</h2>
+		<label>
+			<input type="checkbox"/>
+			<button>x</button>
+		</label>
+	</div>
+</div>
 
 <style>
-	main {
-		position: relative;
-		max-width: 800px;
+	.new-todo {
+		font-size: 1.4em;
+		width: 100%;
+		margin: 2em 0 1em 0;
+	}
+
+	.board {
+		max-width: 36em;
 		margin: 0 auto;
-		min-height: 101vh;
-		padding: 1em;
 	}
 
-	main :global(.meta) {
-		color: #999;
-		font-size: 12px;
-		margin: 0 0 1em 0;
+	.left, .right {
+		float: left;
+		width: 50%;
+		padding: 0 1em 0 0;
+		box-sizing: border-box;
 	}
 
-	main :global(a) {
-		color: rgb(0,0,150);
+	h2 {
+		font-size: 2em;
+		font-weight: 200;
+		user-select: none;
+	}
+
+	label {
+		top: 0;
+		left: 0;
+		display: block;
+		font-size: 1em;
+		line-height: 1;
+		padding: 0.5em;
+		margin: 0 auto 0.5em auto;
+		border-radius: 2px;
+		background-color: #eee;
+		user-select: none;
+	}
+
+	input { margin: 0 }
+
+	.right label {
+		background-color: rgb(180,240,100);
+	}
+
+	button {
+		float: right;
+		height: 1em;
+		box-sizing: border-box;
+		padding: 0 0.5em;
+		line-height: 1;
+		background-color: transparent;
+		border: none;
+		color: rgb(170,30,30);
+		opacity: 0;
+		transition: opacity 0.2s;
+	}
+
+	label:hover button {
+		opacity: 1;
 	}
 </style>
