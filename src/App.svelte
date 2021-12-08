86c882b5eb8bc6dbaba7e5bb07f87cb07c76b2e3
Kim Taenam <cjb940103@gmail.com>
2021-12-08 18:32:59 +0900

    todo add and add isEdit in todos

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script>
-	import { quintOut } from 'svelte/easing';
+	import Todo from "./Todo.svelte";
 	import { crossfade } from 'svelte/transition';
-	import { flip } from 'svelte/animate';
+    import { quintOut } from 'svelte/easing';
 
 	const [send, receive] = crossfade({
 		fallback(node, params) {
@@ -20,12 +20,12 @@
 	})
 
 	let todos = [
-		{ id: 1, done: false, description: 'write some docs' },
-		{ id: 2, done: false, description: 'start writing JSConf talk' },
-		{ id: 3, done: true, description: 'buy some milk' },
-		{ id: 4, done: false, description: 'mow the lawn' },
-		{ id: 5, done: false, description: 'feed the turtle' },
-		{ id: 6, done: false, description: 'fix some bugs' },
+		{ id: 1, done: false, description: 'write some docs', isEdit: false },
+		{ id: 2, done: false, description: 'start writing JSConf talk', isEdit: false },
+		{ id: 3, done: true, description: 'buy some milk', isEdit: false },
+		{ id: 4, done: false, description: 'mow the lawn', isEdit: false },
+		{ id: 5, done: false, description: 'feed the turtle', isEdit: false },
+		{ id: 6, done: false, description: 'fix some bugs', isEdit: false },
 	];
 
 	let uid = todos.length + 1;
@@ -35,6 +35,7 @@
 			id: uid++,
 			done: false,
 			description: input.value,
+			isEdit: false
 		};
 
 		todos = [todo, ...todos];
@@ -56,36 +57,20 @@
 
 	<div class="left">
 		<h2>todo</h2>
-		{#each todos.filter(t => !t.done) as todo (todo.id)}
-			<label
-				in:receive="{{key: todo.id}}"
-				out:send="{{key: todo.id}}"
-				animate:flip
-			>
-				<input type="checkbox" bind:checked={todo.done}/>
-				{todo.description}
-				<button on:click="{() => remove(todo)}">remove</button>
-			</label>
+		{#each todos?.filter(t => !t.done) as todo (todo.id)}
+			<Todo {todo} {send} {receive} {remove}/>
 		{/each}
 	</div>
 
 	<div class="right">
 		<h2>done</h2>
-		{#each todos.filter(t => t.done) as todo (todo.id)}
-		<label
-			in:receive="{{key: todo.id}}"
-			out:send="{{key: todo.id}}"
-			animate:flip
-		>
-			<input type="checkbox" bind:checked={todo.done}/>
-			{todo.description}
-			<button on:click="{() => remove(todo)}">remove</button>
-		</label>
+		{#each todos?.filter(t => t.done) as todo (todo.id)}
+			<Todo {todo} {send} {receive} {remove}/>
 		{/each}
 	</div>
 </div>
 
-<style>
+<style global>
 	.new-todo {
 		font-size: 1.4em;
 		width: 100%;
@@ -110,42 +95,5 @@
 		user-select: none;
 	}
 
-	label {
-		position: relative;
-		top: 0;
-		left: 0;
-		display: block;
-		font-size: 1em;
-		line-height: 1;
-		padding: 0.5em;
-		margin: 0 auto 0.5em auto;
-		border-radius: 2px;
-		background-color: #eee;
-		user-select: none;
-	}
-
 	input { margin: 0 }
-
-	.right label {
-		background-color: rgb(180,240,100);
-	}
-
-	button {
-		position: absolute;
-		top: 0;
-		right: 0.2em;
-		width: 2em;
-		height: 100%;
-		background: no-repeat 50% 50% url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath fill='%23676778' d='M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M17,7H14.5L13.5,6H10.5L9.5,7H7V9H17V7M9,18H15A1,1 0 0,0 16,17V10H8V17A1,1 0 0,0 9,18Z'%3E%3C/path%3E%3C/svg%3E");
-		background-size: 1.4em 1.4em;
-		border: none;
-		opacity: 0;
-		transition: opacity 0.2s;
-		text-indent: -9999px;
-		cursor: pointer;
-	}
-
-	label:hover button {
-		opacity: 1;
-	}
 </style>
---
+++ src/Todo.svelte
@@ -0,0 +1,56 @@
+<script>
+	import { flip } from 'svelte/animate';
+    import { quintOut } from 'svelte/easing';
+	import { crossfade } from 'svelte/transition';
+
+    export let todo;
+    export let receive;
+    export let send;
+    export let remove;
+
+    console.log(todo);
+</script>
+
+<label
+    in:receive="{{key: todo.id}}"
+    out:send="{{key: todo.id}}"
+>
+    <input type="checkbox" bind:checked={todo.done}/>
+    {todo.description}
+    <button on:click="{() => remove(todo)}">remove</button>
+</label>
+
+<style>
+    label {
+		position: relative;
+		top: 0;
+		left: 0;
+		display: block;
+		font-size: 1em;
+		line-height: 1;
+		padding: 0.5em;
+		margin: 0 auto 0.5em auto;
+		border-radius: 2px;
+		background-color: #eee;
+		user-select: none;
+	}
+
+    button {
+		position: absolute;
+		top: 0;
+		right: 0.2em;
+		width: 2em;
+		height: 100%;
+		background: no-repeat 50% 50% url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath fill='%23676778' d='M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M17,7H14.5L13.5,6H10.5L9.5,7H7V9H17V7M9,18H15A1,1 0 0,0 16,17V10H8V17A1,1 0 0,0 9,18Z'%3E%3C/path%3E%3C/svg%3E");
+		background-size: 1.4em 1.4em;
+		border: none;
+		opacity: 0;
+		transition: opacity 0.2s;
+		text-indent: -9999px;
+		cursor: pointer;
+	}
+
+	label:hover button {
+		opacity: 1;
+	}
+</style>
