79e0b8067b007912dffbe7180b83779c79b6f082
kimtaenam <cjb940103@gmail.com>
2021-12-12 23:28:32 +0900

    todo count with afterUpdate lifecycle

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Todo from "./Todo.svelte";
 	import { writable } from "svelte/store";
+	import { afterUpdate } from 'svelte';
 
 	let todos = writable([
 		{ id: 1, done: false, description: 'write some docs'},
@@ -27,27 +28,43 @@
 		$todos = [todo, ...$todos];
 		input.value = '';
 	};
+
+	let todoCount;
+	let doneCount;
+
+	afterUpdate(() => {
+		todoCount = $todos?.filter(t => !t.done).length;
+		doneCount = $todos?.filter(t => t.done).length;
+	});
 </script>
 
-<div class="board">
-	<input 
-		class="new-todo"
-		placeholder="what needs to be done?"
-		on:keydown={e => e.key === "Enter" && add(e.target)}
-	/>
-
-	<div class="left">
-		<h2>todo</h2>
-		{#each $todos?.filter(t => !t.done) as todo (todo.id)}
-			<Todo {todos} {todo} />
-		{/each}
-	</div>
-
-	<div class="right">
-		<h2>done</h2>
-		{#each $todos?.filter(t => t.done) as todo (todo.id)}
-			<Todo {todos} {todo} class="done"/>
-		{/each}
+<div class="wrapper">
+	<div class="board">
+		<input 
+			class="new-todo"
+			placeholder="what needs to be done?"
+			on:keydown={e => e.key === "Enter" && add(e.target)}
+		/>
+	
+		<div class="left">
+			<div class="title">
+				<h2>todo </h2>
+				<span>count {todoCount}</span>
+			</div>
+			{#each $todos?.filter(t => !t.done) as todo (todo.id)}
+				<Todo {todos} {todo} />
+			{/each}
+		</div>
+	
+		<div class="right">
+			<div class="title">
+				<h2>done </h2>
+				<span>count {doneCount}</span>
+			</div>
+			{#each $todos?.filter(t => t.done) as todo (todo.id)}
+				<Todo {todos} {todo} class="done"/>
+			{/each}
+		</div>
 	</div>
 </div>
 
@@ -68,6 +85,12 @@
 		width: 50%;
 		padding: 0 1em 0 0;
 		box-sizing: border-box;
+	}
+
+	.title {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
 	}
 
 	h2 {
