c5639f399e23c614294187a050e12380d4ad00a1
Kim Taenam <cjb940103@gmail.com>
2021-12-20 13:22:49 +0900

    typescript error 수정

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,6 @@ import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
-import sveltePreprocess from 'svelte-preprocess';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -41,12 +40,12 @@ export default {
 	},
 	plugins: [
 		svelte({
-			preprocess: sveltePreprocess({ sourceMap: !production }),
 			compilerOptions: {
 				// enable run-time checks when not in production
 				dev: !production
 			},
 			preprocess: sveltePreprocess({
+				sourceMap: !production,
 				babel: {
 				  	presets: [
 						["@babel/preset-env", {
